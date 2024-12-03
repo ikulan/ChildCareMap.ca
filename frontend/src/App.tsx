89e2069d8c75e8c54db15ec447174b9ff46b57d7
ikulan <ikulan12@gmail.com>
2024-12-03 11:32:37 -0800

    Add Tailwind CSS and enable prettier formatter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,19 @@
-import Map from 'react-map-gl';
+import Map from "react-map-gl";
 
 export default function App() {
   return (
-    <Map
-      mapboxAccessToken={import.meta.env.VITE_MAPBOX_API_TOKEN}
-      initialViewState={{
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 14
-      }}
-      style={{width: 600, height: 400}}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-    />
+    <>
+      <h1 className="p-4 text-3xl font-bold underline">Hello Tailwind</h1>
+      <Map
+        mapboxAccessToken={import.meta.env.VITE_MAPBOX_API_TOKEN}
+        initialViewState={{
+          longitude: -122.4,
+          latitude: 37.8,
+          zoom: 14,
+        }}
+        style={{ width: 600, height: 400 }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      />
+    </>
   );
 }
