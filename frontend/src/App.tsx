3f62cd0b58b91b3eef418a55a1619408b1dc7381
ikulan <ikulan12@gmail.com>
2024-12-05 16:42:12 -0800

    Minor fixes
    - fix some warning message regarding svg
    - set max width of the app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import Header from "./components/header/Header";
 
 export default function App() {
   return (
-    <div className="h-svh">
+    <div className="m-auto h-svh max-w-screen-xl">
       <Header />
       <main className="mb-2 grid h-5/6 grid-cols-4 grid-rows-1">
         <DaycareList />
--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -5,7 +5,7 @@ import SiteLogo from "./SiteLogo";
 function Header() {
   return (
     <header className="border-b border-gray-200 px-4 py-2.5">
-      <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between">
+      <div className="flex flex-wrap items-center justify-between">
         <SiteLogo />
         <SearchBar />
         <Hamburger />
