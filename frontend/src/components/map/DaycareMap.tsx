5fb535562f0fe18e247b69b8aa8141e6dfd13a17
ikulan <ikulan12@gmail.com>
2024-12-11 11:47:21 -0800

    Update map dynamically with different city data

--- frontend/src/components/map/DaycareMap.tsx
+++ frontend/src/components/map/DaycareMap.tsx
@@ -8,13 +8,24 @@ import PopupInfo from "./PopupInfo";
 import { layerIds } from "../../services/mapbox/layers";
 import type { Location } from "../../types/interfaces";
 
-const geojsonUrl = import.meta.env.VITE_DATA_URL + "/port_coquitlam.geojson";
-
 function DaycareMap() {
+  const [city, setCity] = useState<string>("port_coquitlam");
   const [selectedLocation, setSelectedLocation] = useState<Location | null>(
     null,
   );
   const mapRef = useRef<MapRef>(null);
+
+  const changeCity = (cityObj) => {
+    // load city data
+    setCity(cityObj.name);
+
+    // move map center
+    mapRef.current?.easeTo({
+      center: [cityObj.longitude, cityObj.latitude],
+      zoom: cityObj.zoom,
+      duration: 500,
+    });
+  };
 
   const onClick = (event) => {
     event.originalEvent.stopPropagation();
@@ -76,10 +87,37 @@ function DaycareMap() {
         ref={mapRef}
       >
         <MapControls />
-        <MapSource sourceUrl={geojsonUrl} />
-
+        <MapSource city={city} />
         <PopupInfo location={selectedLocation} onClose={setSelectedLocation} />
       </Map>
+      <button
+        type="button"
+        className="m-2 rounded-lg bg-purple-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-purple-800 focus:outline-none"
+        onClick={() => {
+          changeCity({
+            name: "coquitlam",
+            longitude: -122.79413,
+            latitude: 49.27962,
+            zoom: 12,
+          });
+        }}
+      >
+        Coquitlam
+      </button>
+      <button
+        type="button"
+        className="m-2 rounded-lg bg-purple-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-purple-800 focus:outline-none"
+        onClick={() => {
+          changeCity({
+            name: "port_coquitlam",
+            longitude: -122.75106,
+            latitude: 49.269857,
+            zoom: 13.5,
+          });
+        }}
+      >
+        Port Coquitlam
+      </button>
     </div>
   );
 }
--- frontend/src/components/map/MapSource.tsx
+++ frontend/src/components/map/MapSource.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Layer, Source } from "react-map-gl";
 import {
   clusterLayer,
@@ -5,12 +6,27 @@ import {
   unclusteredPointLayer,
 } from "../../services/mapbox/layers";
 
-function MapSource({ sourceUrl }: { sourceUrl: string }) {
+const sourceUrl = (city: string): string => {
+  return `${import.meta.env.VITE_DATA_URL}/data/${city}.geojson`;
+};
+
+function MapSource({ city }: { city: string }) {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const url = sourceUrl(city);
+    /* global fetch */
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((json) => setData(json))
+      .catch((err) => console.error("Could not load data", err)); // eslint-disable-line
+  }, [city]);
+
   return (
     <Source
       id="my-data"
       type="geojson"
-      data={sourceUrl}
+      data={data}
       cluster={true}
       clusterRadius={50}
       clusterMinPoints={5}
