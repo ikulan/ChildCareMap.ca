da56b44418c1c1494551916008449056c9c282bc
ikulan <ikulan12@gmail.com>
2024-12-16 09:51:55 -0800

    Add site logo and favicon

--- frontend/src/components/SiteLogo.tsx
+++ frontend/src/components/SiteLogo.tsx
@@ -9,7 +9,12 @@ function SiteLogo() {
         target="_blank"
         rel="noreferrer"
       >
-        <span className="self-center whitespace-nowrap text-2xl font-semibold text-gray-600">
+        <img
+          src="/android-chrome-192x192.png"
+          className="h-8"
+          alt="Site Logo"
+        />
+        <span className="ms-2 self-center whitespace-nowrap text-2xl font-semibold text-gray-600">
           ChildCareMap.ca
         </span>
       </a>
