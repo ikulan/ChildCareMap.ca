cc2d627b332f1bbb2be84ac007410c8db618fb23
ikulan <ikulan12@gmail.com>
2025-01-10 16:50:36 -0800

    Add package: React Router
    Integrate  [React Router](https://reactrouter.com/) for Enhanced SEO and URL Management.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,16 @@
-import DaycareMap from "./components/map/DaycareMap";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import { MapProvider } from "react-map-gl";
-import RegionModel from "./components/options/RegionModel";
-import useConfigStore from "./stores/configStore";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Map from "./pages/Map.tsx";
+import About from "./pages/About.tsx";
 
-export default function App() {
-  const regionModalIsOpen = useConfigStore((state) => state.regionModalIsOpen);
-  const closeRegionModal = useConfigStore((state) => state.closeRegionModal);
-
+function App() {
   return (
-    <div className="m-auto h-svh max-w-screen-xl">
-      <MapProvider>
-        <Header />
-        {regionModalIsOpen && (
-          <RegionModel isOpen={regionModalIsOpen} onClose={closeRegionModal} />
-        )}
-        <main className="mb-2 grid h-5/6 grid-cols-3 grid-rows-1 px-4">
-          <DaycareMap />
-        </main>
-        <Footer />
-      </MapProvider>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Map />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+export default App;
---
+++ frontend/src/pages/Map.tsx
@@ -0,0 +1,26 @@
+import DaycareMap from "../components/map/DaycareMap";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import { MapProvider } from "react-map-gl";
+import RegionModel from "../components/options/RegionModel";
+import useConfigStore from "../stores/configStore";
+
+export default function Map() {
+  const regionModalIsOpen = useConfigStore((state) => state.regionModalIsOpen);
+  const closeRegionModal = useConfigStore((state) => state.closeRegionModal);
+
+  return (
+    <div className="m-auto h-svh max-w-screen-xl">
+      <MapProvider>
+        <Header />
+        {regionModalIsOpen && (
+          <RegionModel isOpen={regionModalIsOpen} onClose={closeRegionModal} />
+        )}
+        <main className="mb-2 grid h-5/6 grid-cols-3 grid-rows-1 px-4">
+          <DaycareMap />
+        </main>
+        <Footer />
+      </MapProvider>
+    </div>
+  );
+}
